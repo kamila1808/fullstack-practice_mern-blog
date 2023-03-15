782e208f3cb944ff726f56ae0584ab424ef8fa99
Kamila Nauryzbayeva <kamila.nryzbv@gmail.com>
2023-03-15 17:15:28 +0600

    страницы регистрации и авторизации

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const LoginPage = () => {
+  return (
+    <form
+      onSubmit={(e) => e.preventDefault()}
+      className="w-1/4 h-60 mx-auto mt-40"
+    >
+      <h1 className="text-lg text-white text-center mb-5">Авторизация</h1>
+      <label className="text-s text-gray-400">
+        Имя пользователя:
+        <input
+          type="text"
+          placeholder="Введите имя пользователя"
+          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-s outline-none placeholder:text-gray-700 mb-5"
+        />
+      </label>
+      <label className="text-s text-gray-400">
+        Пароль:
+        <input
+          type="text"
+          placeholder="Введите пароль"
+          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-s outline-none placeholder:text-gray-700 mb-5"
+        />
+      </label>
+      <div className="flex gap-8 justify-center mt-4">
+        <button
+          type="submit"
+          className="flex justify-center items-center text-s text-white rounded-sm py-w px-4 bg-gray-600"
+        >
+          Войти
+        </button>
+        <Link
+          to="/register"
+          className="flex justify-center items-center text-s text-white"
+        >Нет аккаунта?</Link>
+      </div>
+    </form>
+  );
+};
